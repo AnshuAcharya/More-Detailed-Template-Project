9e762d6d0c0a246b600faac4c75a7edc0d297862
Anshu <acharya.anshu@gmail.com>
2025-04-08 20:15:44 -0700

    spotless formatting

--- src/main/java/frc/robot/subsystems/SingleJointedArm.java
+++ src/main/java/frc/robot/subsystems/SingleJointedArm.java
@@ -1,5 +1,3 @@
 package frc.robot.subsystems;
 
-public class SingleJointedArm {
-     
-}
+public class SingleJointedArm {}
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,3 +1,3 @@
 package frc.robot.subsystems;
 
-public class Elevator {}
+public class Turret {}
